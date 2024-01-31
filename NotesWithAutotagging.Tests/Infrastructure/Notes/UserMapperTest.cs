65a0c7cc5a203a6ccb005ffc36568431bce5d5b4
Jakub Siwak <jakub.siwak@compilon.com>
2024-01-31 10:39:00 +0100

    User mapper test

---
+++ NotesWithAutotagging.Tests/Infrastructure/Notes/UserMapperTest.cs
@@ -0,0 +1,27 @@
+﻿using NotesWithAutotagging.Database.Models;
+using NotesWithAutotagging.Infrastructure.Notes;
+using NotesWithAutotagging.Infrastructure.Users;
+
+namespace NotesWithAutotagging.Tests.Infrastructure.Notes
+{
+    [TestFixture]
+    public class UserMapperTest
+    {
+        [Test]
+        public void UserMapper_FromDatabaseUser_ToContractsNote()
+        {
+            var userDb = new Database.Models.User
+            {
+                Id = 1,
+                Name = "dev",
+                Password = "123456"
+            };
+
+            var user = userDb.ToContractUser();
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual(userDb.Id, user.Id);
+            Assert.AreEqual(userDb.Name, user.Name);
+        }
+    }
+}
